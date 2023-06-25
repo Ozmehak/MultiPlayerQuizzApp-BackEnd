c4582de93b989673cca193e3e154db18981ad0f3
calle <ozmehak.calle@gmail.com>
2023-06-25 17:09:35 +0200

    Project Startup

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO
+import os
 
+SECRET_KEY = os.getenv("SECRET_KEY")
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = SECRET_KEY
 socketio = SocketIO(app)
 
 if __name__ == '__main__':
     socketio.run(app)
+
+
+def test():
+    print("test")
